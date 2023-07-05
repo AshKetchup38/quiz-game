5c95b2224f9d1ba4b9ef49696fb1c1a295c28c37
AshKetchup38 <135846103+AshKetchup38@users.noreply.github.com>
2023-07-05 10:24:16 +0800

    Check user input

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
-THEME_COLOR = "#375362"
+THEME_COLOR = "#4E31AA"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
@@ -13,15 +15,35 @@ class QuizInterface:
         self.score.grid(column=1,row=0)
 
         self.canvas = Canvas(width=300,height=250, bg="white", highlightthickness=0)
-        self.text = self.canvas.create_text(150, 125, text="Text goes here", fill="black", font=("Arial",20,"italic"))
+        self.text = self.canvas.create_text(150, 125, text="Text goes here", fill="black", font=("Arial",20,"italic"), width=280)
         self.canvas.grid(column=0,row=1,columnspan=2, pady=50)
 
         wrong = PhotoImage(file="images/false.png")
-        self.wrong_btn = Button(image=wrong, highlightthickness=0)
+        self.wrong_btn = Button(image=wrong, highlightthickness=0, command=self.check_true)
         self.wrong_btn.grid(column=1, row=2)
 
         right = PhotoImage(file="images/true.png")
-        self.right_btn = Button(image=right, highlightthickness=0)
+        self.right_btn = Button(image=right, highlightthickness=0, command=self.check_false)
         self.right_btn.grid(column=0, row=2)
 
+        self.get_question()
+
         self.window.mainloop()
+
+    def get_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.text, text=q_text)
+
+    def check_true(self):
+        answer = self.quiz.check_answer("true")
+        if answer is True:
+            print("Correct")
+        else:
+            print("False")
+
+    def check_false(self):
+        answer = self.quiz.check_answer("false")
+        if answer is True:
+            print("Correct")
+        else:
+            print("False")
