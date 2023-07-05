f755914168e645174ccc4dba1e52753854affb19
AshKetchup38 <135846103+AshKetchup38@users.noreply.github.com>
2023-07-05 09:46:19 +0800

    Added UI

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import requests
 
-response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
-question_data = response.json()['results']
-print(question_data)
+parameters = {
+    "amount":10, 
+    "type":"boolean"
+}
 
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+question_data = response.json()['results']
